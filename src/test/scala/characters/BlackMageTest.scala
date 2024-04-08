259e6bd8eeef442bc66d6ad6739d9bb77f9e72d2
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-08 10:04:12 -0400

    test: Added test class for BlackMage character

---
+++ src/test/scala/characters/BlackMageTest.scala
@@ -0,0 +1,18 @@
+class BlackMageTest extends munit.FunSuite {
+    var testBlackMage = Option[BlackMage] = None
+
+    test("Constructor test") {
+        testBlackMage = Some(new BlackMage("BlackMage 1", 100.0, 50.0, 70.1, 200))
+        assert(testBlackMage.isDefined)
+        val blackMage = testBlackMage.get
+        assertEquals(blackMage.name, "BlackMage 1")
+        assertEquals(blackMage.healthPoints, 100.0)
+        assertEquals(blackMage.defense, 50.0)
+        assertEquals(blackMage.weight, 70.1)
+        assertEquals(blackMage.manaPoints, 200.0)
+
+        assertEquals(testBlackMage.weapon, None)
+
+    }
+
+}

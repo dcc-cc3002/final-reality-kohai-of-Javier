668ea350e5660c51d60ed402c57e889f11491d4d
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-10 05:28:26 -0400

    refactor: changed name of the Enemy trait and class

--- src/main/scala/Enemy.scala
+++ src/main/scala/Enemy.scala
@@ -1,7 +1,6 @@
-trait EnemyTrait {
-    val name: String
-    var healthPoints: Double
-    val attack: Double
-    val defense: Double
-    val weight: Double
+import weapons.{Weapon, AuxiliaryWeapon}
+import characters.CharacterTrait
+
+class Enemy(val name: String, var healthPoints: Double, val attack: Double, val defense: Double, val weight: Double) extends CharacterTrait {
+    var weapon: Option[Weapon] = Some(new AuxiliaryWeapon("attack", attack, 0, this))
 }

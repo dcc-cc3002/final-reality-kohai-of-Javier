8d8652af4f4ceededf8459b07d503b38fa094bbc
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:32:16 -0400

    docs: Add Scaladoc comment

--- src/main/scala/weapons/Weapon.scala
+++ src/main/scala/weapons/Weapon.scala
@@ -1,6 +1,12 @@
 package weapons
 import characters.CharacterTrait
 
+/** Trait for Weapons
+ * @param name Name of the weapon
+ * @param attackPoints Number of attack points of the weapon
+ * @param weight Weight of the weapon
+ * @param owner Character that owns the weapon
+ */
 trait Weapon {
     val name: String
     val attackPoints: Double

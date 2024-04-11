09255b8386dfc2d66bb11c09649a6192ad66f71b
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:03:36 -0400

    docs: Add comment for AbstractCommonCharacter class

--- src/main/scala/characters/AbstractCommonCharacter.scala
+++ src/main/scala/characters/AbstractCommonCharacter.scala
@@ -1,5 +1,8 @@
 package characters
 
+/** Abstract class for a common character
+ * @see AbstractCharacter
+ */
 abstract class CommonCharacter(name: String, healthPoints: Double, defense: Double, weight: Double) extends AbstractCharacter(name, healthPoints, defense, weight) {
 
 }

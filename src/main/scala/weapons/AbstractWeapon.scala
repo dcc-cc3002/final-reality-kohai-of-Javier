859edb01b900d28fe4fd6ad1bafb2cf734ca2861
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:26:39 -0400

    docs: Add Scaladoc comment

--- src/main/scala/weapons/AbstractWeapon.scala
+++ src/main/scala/weapons/AbstractWeapon.scala
@@ -1,6 +1,13 @@
 package weapons
 import characters.CharacterTrait
 
+/** Abstract class for a weapon.
+ *
+ * @param name The name of the weapon
+ * @param attackPoints number of attack points of the weapon
+ * @param weight The weight of the weapon
+ * @param owner A Character that owns the weapon
+ */
 abstract class AbstractWeapon(val name: String, val attackPoints: Double, val weight: Double, var owner: CharacterTrait) extends Weapon {
 }
 

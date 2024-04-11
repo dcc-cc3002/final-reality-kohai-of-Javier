07da4518a6d618bbb8343fc507c596ebfb691d37
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:29:40 -0400

    docs: Add Scaladoc comment

--- src/main/scala/weapons/MagicWeapon.scala
+++ src/main/scala/weapons/MagicWeapon.scala
@@ -1,5 +1,8 @@
 package weapons
 
+/** Trait for a Magical Weapon
+ * @param magicAttack Number of the magical attack points of the weapon
+ */
 trait MagicWeapon extends Weapon {
     val magicAttack: Double
 }

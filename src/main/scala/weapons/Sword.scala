9a32a6cffce622c6b9c64ead07185a3f4e869c30
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:34:05 -0400

    docs: Add Scaladoc comment

--- src/main/scala/weapons/Sword.scala
+++ src/main/scala/weapons/Sword.scala
@@ -1,5 +1,8 @@
 package weapons
 import characters.CharacterTrait
 
+/** Class for Sword ordinary weapon.
+ * @see AbstractWeapon
+ */
 class Sword(name: String, attackPoints: Double, weight: Double, owner: CharacterTrait) extends AbstractWeapon(name, attackPoints, weight, owner) {
 }

5457a0de2bba3f4375e942034851bbe017d19ead
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:21:42 -0400

    docs: Add Scaladoc comment

--- src/main/scala/characters/Ninja.scala
+++ src/main/scala/characters/Ninja.scala
@@ -1,4 +1,5 @@
 package characters
 
+/** Class for Ninja Common Character */
 class Ninja(name: String, healthPoints: Double, defense: Double, weight: Double) extends CommonCharacter(name, healthPoints, defense, weight) {
 }

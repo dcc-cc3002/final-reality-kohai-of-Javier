1b9ea1f3ba9c970f3977744e1d485506cea97c9b
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 16:59:13 -0400

    docs: Add ScalaDoc comment for PartyTrait

--- src/main/scala/PartyTrait.scala
+++ src/main/scala/PartyTrait.scala
@@ -1,5 +1,9 @@
 import characters.CharacterTrait
 
+/** The trait for a Party.
+  * A party contains zero or more characters
+  * If it has zero characters, it is considered defeated
+  */
 trait PartyTrait {
     def addCharacter(x: CharacterTrait): Unit
     def isDefeated(): Boolean

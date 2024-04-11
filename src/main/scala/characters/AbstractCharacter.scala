de132d3eff62d9802d6669fdb8d18fbbfe1169ad
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:01:22 -0400

    docs: Add ScalaDoc comment for AbstractCharacter

--- src/main/scala/characters/AbstractCharacter.scala
+++ src/main/scala/characters/AbstractCharacter.scala
@@ -1,6 +1,12 @@
 package characters
 import weapons.Weapon
 
+/** An abstract class for a Character
+ * @param name The name of the character
+ * @param healthPoints The number of initial health points of the character
+ * @param defense The defense of the character
+ * @param weight The weight of the character
+ */
 abstract class AbstractCharacter(val name: String, var healthPoints: Double, val defense: Double, val weight: Double) extends CharacterTrait {
     var weapon: Option[Weapon] = None
 }

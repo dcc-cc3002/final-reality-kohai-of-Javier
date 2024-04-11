e8a471f12e129cac26d49b94673de5b5ea0c71df
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:24:55 -0400

    docs: Add Scaladoc comment

--- src/main/scala/weapons/AbstractMagicWeapon.scala
+++ src/main/scala/weapons/AbstractMagicWeapon.scala
@@ -1,6 +1,12 @@
 package weapons
 import characters.CharacterTrait
 
+/** Abstract class for magical weapon.
+ * The main difference with an ordinary weapon is that
+ * the magical weapons has a magic attack
+ *
+ * @see AbstractWeapon
+ */
 abstract class AbstractMagicWeapon(val name: String, val attackPoints: Double, val magicAttack: Double, val weight: Double, var owner: CharacterTrait) extends MagicWeapon {
 
 }

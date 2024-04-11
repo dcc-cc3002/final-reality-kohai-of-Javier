683545a6d7dd1173f685655031a469df5bb82147
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:30:50 -0400

    docs: Add Scaladoc comment

--- src/main/scala/weapons/Bow.scala
+++ src/main/scala/weapons/Bow.scala
@@ -1,5 +1,8 @@
 package weapons
 import characters.CharacterTrait
 
+/** Class for Staff ordinary weapon.
+ * @see AbstractWeapon
+ */
 class Bow(name: String, attackPoints: Double, weight: Double, owner: CharacterTrait) extends AbstractWeapon(name, attackPoints, weight, owner) {
 }

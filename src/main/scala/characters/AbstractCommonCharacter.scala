73ed34b14fc8b279f72faccbc3bb732e47a74ebe
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-11 17:07:04 -0400

    docs: updated ScalaDoc comment including examples
    Examples of concrete classes: Ninja, Paladin, and Warrior

--- src/main/scala/characters/AbstractCommonCharacter.scala
+++ src/main/scala/characters/AbstractCommonCharacter.scala
@@ -1,6 +1,7 @@
 package characters
 
-/** Abstract class for a common character
+/** Abstract class for a Common Character.
+ * Concrete classes for Common Characters are Paladin, Ninja and Warrior
  * @see AbstractCharacter
  */
 abstract class CommonCharacter(name: String, healthPoints: Double, defense: Double, weight: Double) extends AbstractCharacter(name, healthPoints, defense, weight) {

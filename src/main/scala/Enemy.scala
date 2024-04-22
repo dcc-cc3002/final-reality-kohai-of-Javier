7fcc802dc8256c4455dd389635c0c19784e69638
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-22 09:05:25 -0400

    feat: Implemented maxActionBar method

--- src/main/scala/Enemy.scala
+++ src/main/scala/Enemy.scala
@@ -14,4 +14,6 @@ import characters.CharacterTrait
  */
 class Enemy(val name: String, var healthPoints: Double, val attack: Double, val defense: Double, val weight: Double) extends CharacterTrait {
     var weapon: Option[Weapon] = Some(new AuxiliaryWeapon("attack", attack, 0, this))
+
+    def maxActionBar(): Double = weight
 }

8c8dc67bc86c1875ce085600b85560bb997185fb
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-04-22 09:06:09 -0400

    fix: Changed variable return types and docs fix

--- src/main/scala/Programmer.scala
+++ src/main/scala/Programmer.scala
@@ -1,17 +1,17 @@
 import characters.CharacterTrait
 import scala.collection.mutable
+import scala.collection.immutable.List
 
 /** Trait for the turns programmer, it determines which character plays next.
 * @param addCharacter Adds a character to the task programmer
 * @param removeCharacter Eliminates a character from the task programmer
-* @param maxActionBar Calculates the maximum of the action bar of all characters
+* @param maxActionBar Calculates the maximum of the action bar from all characters
 * @param restartActionBar Restarts the action bar of each character
 * @param augmentActionBar Aguments simultaneously the action bar of all characters by an arbitrary amount
 * @param completedActionBar Indicates whether a character completed their action bar
 * @param throwCompleteCharacters Delivers all characters who completed their action bar, in non-increasing order
 * @param selectCharacter Indicates the only character who plays the turn
-* @param charactersAdded Iterable with the list of characters considered by the turns programmer
-* @param actionBar The value of the action bar for each character
+* @param getCharacters Returns a list of all characters added to the task programmer, with the current value of their action bar.
 */
 trait Programmer {
     def addCharacter(newCharacter: CharacterTrait): Unit
@@ -19,9 +19,9 @@ trait Programmer {
     def maxActionBar(): Double
     def restartActionBar(toRestart: CharacterTrait): Unit
     def augmentActionBar(howMuch: Double): Unit
-    def completedActionBar(myCharacter: CharacterTrait): Unit
-    def throwCompleteCharacters(): mutable.Iterable[CharacterTrait]
+    def completedActionBar(myCharacter: CharacterTrait): Boolean
+    def throwCompleteCharacters(): List[CharacterTrait]
     def selectCharacter(): CharacterTrait
-    var charactersAdded: mutable.Iterable[Double]
-    var actionBar: mutable.Iterable[Double]
+
+    def getCharacters(): List[(Double, CharacterTrait)]
 }

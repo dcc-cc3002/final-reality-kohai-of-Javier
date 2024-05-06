df5fc2bd9dffea2ceb719f691fbd6e9499804f18
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-05-06 17:26:16 -0400

    fix: rename CharacterTrait to TCharacter

--- src/main/scala/weapons/AbstractMagicWeapon.scala
+++ src/main/scala/weapons/AbstractMagicWeapon.scala
@@ -1,5 +1,5 @@
 package weapons
-import characters.CharacterTrait
+import characters.TCharacter
 
 /** Abstract class for magical weapon.
  * The main difference with an ordinary weapon is that
@@ -8,7 +8,7 @@ import characters.CharacterTrait
  * @see AbstractWeapon
  * @param magicAttack Number of magic attack points of the weapon
  */
-abstract class AbstractMagicWeapon(val name: String, val attackPoints: Double, val magicAttack: Double, val weight: Double, var owner: CharacterTrait) extends MagicWeapon {
+abstract class AbstractMagicWeapon(val name: String, val attackPoints: Double, val magicAttack: Double, val weight: Double, var owner: TCharacter) extends MagicWeapon {
 
 }
 

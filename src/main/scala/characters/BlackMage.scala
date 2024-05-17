963cb1c83e4ab94991f87c349bba24211165bbac
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-05-17 15:38:21 -0400

    feat: Add a method to set a Weapon
    Add a method to set a weapon for each character type.

--- src/main/scala/characters/BlackMage.scala
+++ src/main/scala/characters/BlackMage.scala
@@ -1,5 +1,12 @@
 package characters
+import weapons.Weapon
 
 /** Class for Magical Character: Black Mage */
 class BlackMage(name: String, healthPoints: Int, defense: Int, weight: Int, manaPoints: Int) extends AbstractMagicalCharacter(name, healthPoints, defense, weight, manaPoints) {
+
+     /** Setter of the weapon of the character.*/
+     def setWeapon(wp: Weapon): Unit = {
+        wp.checkBlackMage
+        weapon = Some(wp)
+     }
 }
--- src/main/scala/characters/Paladin.scala
+++ src/main/scala/characters/Paladin.scala
@@ -1,5 +1,12 @@
 package characters
+import weapons.Weapon
 
 /** Class for Common Character: Paladin */
 class Paladin(name: String, healthPoints: Int, defense: Int, weight: Int) extends CommonCharacter(name, healthPoints, defense, weight) {
+
+     /** Setter of the weapon of the character.*/
+     def setWeapon(wp: Weapon): Unit = {
+        wp.checkPaladin
+        weapon = Some(wp)
+     }
 }

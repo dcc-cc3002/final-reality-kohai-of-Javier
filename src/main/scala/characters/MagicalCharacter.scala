14223a3faf19189cd99b464714830fb0119da92e
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-05-27 15:45:17 -0400

    feat: Add blackSpell and whiteSpell
    And also update the comments

--- src/main/scala/characters/MagicalCharacter.scala
+++ src/main/scala/characters/MagicalCharacter.scala
@@ -1,9 +1,21 @@
 package characters
 
 /** Trait for Magical Characters.
- * @param getManaPoints Returns the number of mana points of the character
- * For the other parameters, @see WCharacter
+ * Magical Characters also have a magic attack.
+ * For the other methods, @see WCharacter
  */
 trait MagicalCharacter extends WCharacter {
+    /** Returns the number of mana points of the character.*/
     def getManaPoints(): Int
+
+
+    /** The character applies a black spell.
+        @param manaCost The cost of applying the spell
+    */
+    def blackSpell(manaCost: Int): Unit
+
+    /** The character applies a white spell.
+        @param manaCost The cost of applying the spell
+    */
+    def whiteSpell(manaCost: Int): Unit
 }

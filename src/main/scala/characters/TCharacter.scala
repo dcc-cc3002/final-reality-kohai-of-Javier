cde9793771db0904caa37d62e5c9658850717cd3
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-05-27 15:45:46 -0400

    feat: add negativeSpell and positiveSpell
    And also add comments

--- src/main/scala/characters/TCharacter.scala
+++ src/main/scala/characters/TCharacter.scala
@@ -1,23 +1,40 @@
 package characters
 
-/** Trait for a Character
- * @param fullActionBar Returns the value of the action bar to be attained for the character to play
- * @param receiveDamage Function that is called when the character is attacked by another
- * @param getName Returns the name of the character
- * @param getDefense Returns the defense of the character
- * @param getDefense Returns the number of defense points of the character
- * @param getWeight Returns the weight of the character
- * @param getHealthPoints Getter for the number of health points of the character
- */
+/** Generic trait for a Character.*/
 trait TCharacter {
+    /** Returns the value of the action bar to be attained for the character to play.*/
     def fullActionBar(): Int
+
+    /** Attack another character.*/
     def attack(other: TCharacter): Unit
+
+
+    /** Function that is called when the character is attacked by another.*/
     def receiveDamage(attackPoints: Int): Unit
+
+    /** Receive damage from a playable character.*/
     def receiveDamagePlayer(attackPoints: Int): Unit
+
+    /** Receive damage from an enemy.*/
     def receiveDamageEnemy(attackPoints: Int): Unit
 
+
+    /** Getter for the name of the character.*/
     def getName(): String
+
+    /** Getter for the number of defense points of the character.*/
     def getDefense(): Int
+
+    /** Getter for the weight of the character.*/
     def getWeight(): Int
+
+    /** Getter for the number of health points of the character.*/
     def getHealthPoints(): Int
+
+
+    /** Receives a negative spell.*/
+    def negativeSpell: Unit
+
+    /** Receives a positive spell.*/
+    def positiveSpell: Unit
 }

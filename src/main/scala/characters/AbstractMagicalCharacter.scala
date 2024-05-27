7d82a4fb8899d79ec6cf013121d0a431b8ac0cb3
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-05-27 15:43:37 -0400

    feat: add protected method to add spell

--- src/main/scala/characters/AbstractMagicalCharacter.scala
+++ src/main/scala/characters/AbstractMagicalCharacter.scala
@@ -1,5 +1,5 @@
 package characters
-import exceptions.{Require, InvalidStatException}
+import exceptions.{Require, InvalidStatException, NoMagicWeaponException, InsufficientManaException}
 
 /** Abstract class for a Magical Character.
  * A magical character also has mana points
@@ -12,4 +12,18 @@ abstract class AbstractMagicalCharacter(name: String, healthPoints: Int, defense
 
     /** Return the number of mana points of the character.*/
     def getManaPoints(): Int = manaPoints
+
+
+    /** Apply a spell.
+     * It checks whether there is enough mana and a magic weapon is equiped.
+     * @param manaCost The number of manaPoints required to apply the spell.
+     */
+    protected def applySpell(manaCost: Int): Unit = {
+        if(manaCost > manaPoints) throw new InsufficientManaException(manaCost, manaPoints)
+        if(weapon.isDefined == false) throw new NoMagicWeaponException
+        if(weapon.get.isMagical == false) throw new NoMagicWeaponException
+
+        manaPoints -= manaCost
+    }
+
 }

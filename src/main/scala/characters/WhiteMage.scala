0d60854173dca3b85749b47ae637aeb4591b42fc
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-05-27 15:44:24 -0400

    feat: add blackSpell and whiteSpell

--- src/main/scala/characters/WhiteMage.scala
+++ src/main/scala/characters/WhiteMage.scala
@@ -1,5 +1,6 @@
 package characters
 import weapons.Weapon
+import exceptions.InvalidUserException
 
 /** Class for Magical Character: White Mage */
 class WhiteMage(name: String, healthPoints: Int, defense: Int, weight: Int, manaPoints: Int) extends AbstractMagicalCharacter(name, healthPoints, defense, weight, manaPoints) {
@@ -10,4 +11,14 @@ class WhiteMage(name: String, healthPoints: Int, defense: Int, weight: Int, mana
          wp.checkWhiteMage
          weapon = Some(wp)
       }
+
+    /** The character applies a white spell.
+        @param manaCost The cost of applying the spell
+    */
+    def whiteSpell(manaCost: Int) = {
+        applySpell(manaCost)
+    }
+
+    /** The character tries to apply a black spell, but an InvalidUserException is thrown.*/
+    def blackSpell(manaCost: Int) = throw new InvalidUserException("A White Mage can not use a black spell")
 }

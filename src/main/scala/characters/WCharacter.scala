26b63e69eb4e94c38c0aa7c1fcd2a6b548f41d96
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-06-04 18:03:50 -0400

    feat: Add setMagicWeapon

--- src/main/scala/characters/WCharacter.scala
+++ src/main/scala/characters/WCharacter.scala
@@ -1,5 +1,6 @@
 package characters
 import weapons.Weapon
+import weapons.magicweapons.MagicWeapon
 
 /** Trait for a Character with Weapon.
 * It extends TCharacter. The idea is for enemies not to carry Weapons.
@@ -12,6 +13,10 @@ trait WCharacter extends TCharacter {
 
     /** Setter for the character's weapon.*/
     def setWeapon(newWeapon: Weapon): Unit
+
+    /** Setter for the character's magical weapon.
+     * It does nothing and the method is overriden for Magical characters.*/
+    def setMagicWeapon(newWeapon: MagicWeapon): Unit
 
     /** Unequip the character's weapon.*/
     def unsetWeapon(): Unit

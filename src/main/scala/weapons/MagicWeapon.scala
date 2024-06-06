84f4b9947d50239463f783de1462d0e3d9d9db5b
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-06-06 10:30:59 -0400

    refactor: Moved files into their subpackages

--- src/main/scala/weapons/MagicWeapon.scala
+++
@@ -1,7 +0,0 @@
-package weapons
-
-/** Trait for a Magical Weapon.*/
-trait MagicWeapon extends Weapon {
-    /** Getter for the number of magic attack points of the weapon.*/
-    def getMagicAttack(): Int
-}

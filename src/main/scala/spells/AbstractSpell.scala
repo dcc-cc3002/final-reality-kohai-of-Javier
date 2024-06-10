771ac28f6b58b0e991d2512f469a02a64ede5532
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-06-10 09:25:21 -0400

    feat: protected method for random effects

--- src/main/scala/spells/AbstractSpell.scala
+++ src/main/scala/spells/AbstractSpell.scala
@@ -4,14 +4,27 @@ import characters._
 import characters.magicalcharacters._
 import exceptions._
 
+import scala.util.Random
+
 /** Abstract Class for a magic spell.*/
 abstract class AbstractSpell extends Spell {
+
+    /** Determines whether an probable effect will be  applied to the target.
+     * @percentage The probability in percentage of an effect being applied.
+     * */
+    protected def hasEffect(percentage: Int): Boolean = {
+        val rand: Random = new Random
+        if(rand.nextInt() % 100 < percentage)
+            true
+        else
+            false
+    }
 
     /** Executes a negative black spell.
      * If it is not possible, an exception is thrown.
      */
     protected def negativeBlack(user: MagicalCharacter, target: TCharacter, manaCost: Int): Unit = {
-        target.negativeSpell
+        target.negativeSpell()
         user.blackSpell(manaCost)
     }
 
@@ -19,7 +32,7 @@ abstract class AbstractSpell extends Spell {
      * If it is not possible, an exception is thrown.
      */
     protected def negativeWhite(user: MagicalCharacter, target: TCharacter, manaCost: Int): Unit = {
-        target.negativeSpell
+        target.negativeSpell()
         user.whiteSpell(manaCost)
     }
 
@@ -27,7 +40,7 @@ abstract class AbstractSpell extends Spell {
      * If it is not possible, an exception is thrown.
      */
     protected def positiveWhite(user: MagicalCharacter, target: TCharacter, manaCost: Int): Unit = {
-        target.positiveSpell
+        target.positiveSpell()
         user.whiteSpell(manaCost)
     }
 }

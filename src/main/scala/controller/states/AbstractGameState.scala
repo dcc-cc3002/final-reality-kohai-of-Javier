cbda359ea310f0215ce2cf8bec00dbc89e07f896
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-06-11 15:54:30 -0400

    feat: Add methods to know about the current state of the controller

--- src/main/scala/controller/states/AbstractGameState.scala
+++ src/main/scala/controller/states/AbstractGameState.scala
@@ -2,4 +2,27 @@ package controller.states
 
 abstract class AbstractGameState extends GameState {
 
+  /** Checks whether the current state of the controller is ApplyingEffects. */
+  def isApplyingEffects(): Boolean = false
+
+  /** Checks whether the current state of the controller is ChangingWeapon. */
+  def isChangingWeapon(): Boolean = false
+
+  /** Checks whether the current state of the controller is CharacterElimination. */
+  def isCharacterElimination(): Boolean = false
+
+  /** Checks whether the current state of the controller is ChoosingAction. */
+  def isChoosingAction(): Boolean = false
+
+  /** Checks whether the current state of the controller is ChoosingSpell. */
+  def isChoosingSpell(): Boolean = false
+
+  /** Checks whether the current state of the controller is ChoosingTarget. */
+  def isChoosingTarget(): Boolean = false
+
+  /** Checks whether the current state of the controller is DoingAction. */
+  def isDoingAction(): Boolean = false
+
+  /** Checks whether the current state of the controller is TurnProgramming. */
+  def isTurnProgramming(): Boolean = false
 }
--- src/main/scala/controller/states/GameState.scala
+++ src/main/scala/controller/states/GameState.scala
@@ -2,7 +2,33 @@ package controller.states
 
 import controller.GameController
 
+/** Trait for a game state */
 trait GameState {
+
+  /** Changes the state of the game controller */
   def update(controller: GameController) : Unit
 
+  /** Checks whether the current state of the controller is ApplyingEffects. */
+  def isApplyingEffects(): Boolean
+
+  /** Checks whether the current state of the controller is ChangingWeapon. */
+  def isChangingWeapon(): Boolean
+
+  /** Checks whether the current state of the controller is CharacterElimination. */
+  def isCharacterElimination(): Boolean
+
+  /** Checks whether the current state of the controller is ChoosingAction. */
+  def isChoosingAction(): Boolean
+
+  /** Checks whether the current state of the controller is ChoosingSpell. */
+  def isChoosingSpell(): Boolean
+
+  /** Checks whether the current state of the controller is ChoosingTarget. */
+  def isChoosingTarget(): Boolean
+
+  /** Checks whether the current state of the controller is DoingAction. */
+  def isDoingAction(): Boolean
+
+  /** Checks whether the current state of the controller is TurnProgramming. */
+  def isTurnProgramming(): Boolean
 }

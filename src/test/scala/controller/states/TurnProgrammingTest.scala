c2cd9a27ebd52adbdcd9234fc3c684713212356d
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-06-19 18:32:22 -0400

    fix: Now the enemy has the lowest fullActionBar value

--- src/test/scala/controller/states/TurnProgrammingTest.scala
+++ src/test/scala/controller/states/TurnProgrammingTest.scala
@@ -5,7 +5,7 @@ class TurnProgrammingTest extends AbstractGameStateTest {
   test("update test") {
     controller.state = new TurnProgramming
     controller.update
-    assertEquals(controller.turnProgrammer.selectCharacter(), characters.head)
+    assertEquals(controller.turnProgrammer.selectCharacter(), testEnemy)
     assert(controller.state.isApplyingEffects())
   }
 }

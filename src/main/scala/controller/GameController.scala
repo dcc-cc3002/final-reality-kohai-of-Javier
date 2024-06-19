aec84a4e0ff87133663b9a0c91e9bd7f689fc84f
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-06-19 18:35:00 -0400

    feat: Add player's party, win/lose/isFinished methods, and a list for enemies.

--- src/main/scala/controller/GameController.scala
+++ src/main/scala/controller/GameController.scala
@@ -1,24 +1,56 @@
 package controller
 
+import characters.enemies.TEnemy
 import controller.states._
-import programmer.{TProgrammer, Programmer}
-import characters.TCharacter
+import programmer.{Programmer, TProgrammer}
+import characters.WCharacter
+import party.{Party, TParty}
+
+import scala.collection.mutable.ArrayBuffer
 
 /** Class for the game controller.
  * The constructor initializes the game
- * @param characterList The list of characters to be added to the game.
+ * @param characterList The list of characters controller by the player to be added to the game.
+ * @param enemyList The list of enemies to be added to the game.
  * */
-class GameController(private val characterList: List[TCharacter]) {
+class GameController(private[controller] val characterList: List[WCharacter], private[controller] val enemyList: List[TEnemy]) {
   // Current game state
-  var state: GameState = _
-  var turnProgrammer: TProgrammer = new Programmer
+  private[controller] var state: GameState = _
+  private[controller] var turnProgrammer: TProgrammer = _
+  private[controller] var playerParty: TParty = _
 
-  for(char <- characterList)
+  println(characterList.size)
+  if(characterList.size != 3)
+    throw new Exception("Error: The player must start with exactly 3 characters.")
+  if(enemyList.size < 1)
+    throw new Exception("Error: The game must be initialized with at leas one enemy.")
+
+  turnProgrammer = new Programmer
+  playerParty = new Party
+  for(char <- characterList) {
+    turnProgrammer.addCharacter(char)
+    playerParty.addCharacter(char)
+  }
+  for(char <- enemyList)
     turnProgrammer.addCharacter(char)
 
-  state = new TurnProgramming()
+  state = new TurnProgramming
 
-  def update: Unit = state.update(this)
+  /** It updates the state of the controller */
+  private[controller] def update(): Unit = state.update(this)
 
+  /** It shows the game is finished. */
+  private[controller] def isFinished: Boolean = win() || lose()
 
+  /** Returns true if the player lost. */
+  private[controller] def lose(): Boolean = playerParty.isDefeated()
+
+  /** Returns true if the player won. */
+  private[controller] def win(): Boolean = {
+    for(enemy <- enemyList) {
+      if(enemy.getHealthPoints != 0)
+        return false
+    }
+    true
+  }
 }

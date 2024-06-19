f262695bf1281157a1991537d439364792fde31a
aprendiz_de_hacktivista <lucmassa@posteo.net>
2024-06-19 18:31:37 -0400

    fix: Three characters to the player's party and a mock enemy

--- src/test/scala/controller/states/AbstractGameStateTest.scala
+++ src/test/scala/controller/states/AbstractGameStateTest.scala
@@ -1,14 +1,17 @@
 package controller.states
 
-import characters.{TCharacter, Warrior}
+import characters.enemies.{Enemy, TEnemy}
+import characters.{Ninja, Paladin, TCharacter, WCharacter, Warrior}
 import controller.GameController
 
 abstract class AbstractGameStateTest extends munit.FunSuite {
   protected var controller: GameController = _
-  protected var characters: List[TCharacter] = _
+  protected var characters: List[WCharacter] = _
+  protected val testEnemy: TEnemy = new Enemy("Enemy", 50,50,50,50)
+
   override def beforeEach(context: BeforeEach) = {
-    characters = List(new Warrior("Warrior", 100, 50, 70))
-    controller = new GameController(characters)
+    characters = List(new Warrior("Warrior", 100, 50, 70), new Ninja("Ninja", 100, 50, 70), new Paladin("Paladin", 100, 50, 70))
+    controller = new GameController(characters, List(testEnemy))
   }
 
   test("getters test") {
